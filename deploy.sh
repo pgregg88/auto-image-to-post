a622fb82a428e8ff1aedebc169e0409021219f1b
pgregg88 <pgregg@prestongregg.com>
2024-08-05 07:51:42 -0500

    deploy updates

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,16 @@
 LOCAL_PATH="/Users/pgregg/git/auto-image-to-post/"
 
 # Remote path to the files
-REMOTE_PATH="u2258-ldfsciwlr2yh@stage:/home/u2258-ldfsciwlr2yh/www/stage.d3clarity.com/public_html/wp-content/plugins/auto-image-to-post/"
+REMOTE_PATH="stage-rsync:/home/u2258-ldfsciwlr2yh/www/stage.d3clarity.com/public_html/wp-content/plugins/auto-image-to-post/"
 
 # Sync local files to remote server, excluding items listed in .rsyncignore
 rsync -avz --delete  --exclude-from '.rsyncignore' $LOCAL_PATH $REMOTE_PATH
 
-# Print completion message
-echo "Deployment to stage completed successfully."
+# Check if the rsync command was successful
+if [ $? -eq 0 ]; then
+  # Print completion message
+  echo "Deployment to stage completed successfully."
+else
+  # Print error message
+  echo "Deployment to stage failed."
+fi
